4625cb8a1479ad393f5b0785502a46a6d4c3e67b
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2024-09-15 19:28:34 +0100

    OSAKA!!!!

--- osaker/lexer.py
+++ osaker/lexer.py
@@ -22,12 +22,13 @@ class OsakerLexer():
         "OP_DEFINE": r":[oO]",
         "OP_INSPECT": ":<",
         "OP_MATH": r":[mM]",
+        "OP_IMPORT": ":+",
 
         "LITERAL_NUMBER": r"-?\b\d+\b",
         "LITERAL_STRING": r"(['\"])(?:\\.|[^\\])*?\1",
         "LITERAL_BOOL": r"\b(?:yes|yaa|ya|no|nuh|nuhuh)\b",
 
-        "NAME": r"[!a-zA-Z_][a-zA-Z0-9_]*",
+        "NAME": r"[a-zA-Z_][a-zA-Z0-9_!]*",
         "ASSIGN": "<--",
 
         "TYPE": r"[~-][a-zA-Z0-9_]+", # characters starting with either "-" or "~"
